b1ab4ad42725038c76faa2d519ad0c12ab9ea4de
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2021-04-13 15:22:14 +0200

    adding presentation 2
    video for presentation 2 is embedded

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -35,6 +35,17 @@ export class Features extends Component {
                 ))
               : "Loading..."}
           </div>
+          <div className="col-xs-12 col-md-6"> <iframe width="560" height="315" src="https://www.youtube.com/embed/fqDY3AgGccI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <img className="img-responsivee" alt=""/> </div>
+          <div className="row">
+            {this.props.data ? this.props.data.map((d,i) => (
+                  <div  key={`${d.title}-${i}`} className="col-xs-12 col-md-5">
+                    {" "}
+                    <h4>Seccond presentation</h4>
+                    <p>{d.paragraphhh}</p>
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
         </div>
       </div>
       </div>
